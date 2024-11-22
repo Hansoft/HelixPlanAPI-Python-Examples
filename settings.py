c3914d7339035e108895d7ae8c34fbc3f55ab745
atsalkitzis <tsalk.sakis@gmail.com>
2024-11-22 11:10:42 +0100

    fix dev Helix Plan API example URL in settings file as well

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ CONNECTION = {
         'PASSWORD': '',
     },
     'dev': {
-        # Add the URL to the API like https://helixplan.example.com/api/graphql
+        # Add the URL to the API like http://localhost:${port}/graphql or other domain you have set the API on
         'URL': '',
         # The regular Helix Plan username and password to run the script as.
         'USER': '',
